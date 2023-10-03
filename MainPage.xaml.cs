018fe6495f02fd364e74f738aff8705e5f04e340
visual <visual@LAB17-13>
2023-10-03 08:31:32 +0300

    uj

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,9 +9,36 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	//private void OnCounterClicked(object sender, EventArgs e)
-	//{
-	
-	//}
+    private void History_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if ((sender as RadioButton).IsChecked)
+        {
+            DisplayAlert("Заголовок", "1,2 курс", "ОК");
+        }
+    }
+
+    private void Cultural_Studies_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if ((sender as RadioButton).IsChecked)
+        {
+            DisplayAlert("Заголовок", "3,4 курс", "ОК");
+        }
+    }
+
+    private void Philosophy_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if ((sender as RadioButton).IsChecked)
+        {
+            DisplayAlert("Заголовок", "1,2,3,4 курс", "ОК");
+        }
+    }
+
+    private void Foreign_languagery_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if ((sender as RadioButton).IsChecked)
+        {
+            DisplayAlert("Заголовок", "4,3 курс", "ОК");
+        }
+    }
 }
 
--- NewPage1.xaml.cs
+++ NewPage1.xaml.cs
@@ -2,8 +2,35 @@ namespace MauiApp_4_Kosichkin;
 
 public partial class NewPage1 : ContentPage
 {
-	public NewPage1()
-	{
-		InitializeComponent();
-	}
+    public NewPage1()
+    {
+        InitializeComponent();
+    }
+    // для окна с кнопкой 
+   
+
+   
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        if (History.IsChecked == true)
+        {
+            Entry.Text = "1,2 Course";
+        }
+
+        if (Cultural_Studies.IsChecked == true)
+        {
+            Entry.Text = "3,4 Course";
+        }
+
+        if (Philosophy.IsChecked == true)
+        {
+            Entry.Text = "4,3 Course";
+        }
+
+        if (Foreign_languagery.IsChecked == true)
+        {
+            Entry.Text = "1,2,3,4 Course";
+        }
+    }
 }
